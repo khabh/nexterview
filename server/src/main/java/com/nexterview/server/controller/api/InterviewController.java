7fd1fa0420386ead285fa7c10bb4a56e8d917d58
juha <khabh@naver.com>
2025-03-30 20:17:29 +0900

    fix: ApiInterviewRequest 검증 추가

--- server/src/main/java/com/nexterview/server/controller/api/InterviewController.java
+++ server/src/main/java/com/nexterview/server/controller/api/InterviewController.java
@@ -4,6 +4,7 @@ import com.nexterview.server.controller.api.dto.request.ApiInterviewRequest;
 import com.nexterview.server.service.InterviewService;
 import com.nexterview.server.service.dto.request.InterviewRequest;
 import com.nexterview.server.service.dto.response.InterviewDto;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,7 +20,7 @@ public class InterviewController {
     private final InterviewService interviewService;
 
     @PostMapping("/prompts/{promptId}/interviews")
-    public InterviewDto saveInterview(@PathVariable Long promptId, @RequestBody ApiInterviewRequest apiRequest) {
+    public InterviewDto saveInterview(@PathVariable Long promptId, @Valid @RequestBody ApiInterviewRequest apiRequest) {
         InterviewRequest interviewRequest = new InterviewRequest(apiRequest.title(), promptId,
                 apiRequest.promptAnswers(), apiRequest.dialogues());
 
