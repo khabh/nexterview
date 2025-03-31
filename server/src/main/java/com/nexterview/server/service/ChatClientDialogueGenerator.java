70b8bdeeefcd0d7d17e9426f922cd1a1b5ed37ba
juha <khabh@naver.com>
2025-03-31 20:25:58 +0900

    feat: 로컬 테스트를 위한 SimulatedDialogueGenerator 구현

---
+++ server/src/main/java/com/nexterview/server/service/ChatClientDialogueGenerator.java
@@ -0,0 +1,29 @@
+package com.nexterview.server.service;
+
+import com.nexterview.server.domain.CustomizedPrompt;
+import com.nexterview.server.service.dto.response.GeneratedDialogueDto;
+import java.util.List;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ChatClientDialogueGenerator implements DialogueGenerator {
+
+    private final ChatClient chatClient;
+
+    public ChatClientDialogueGenerator(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder
+                .defaultSystem("당신은 개발자 면접을 준비를 위해 한국어로 3개의 예상 질문과 각 질문에 대한 예시 답변을 생성해 주는 역할입니다.")
+                .build();
+    }
+
+    public List<GeneratedDialogueDto> generate(CustomizedPrompt customizedPrompt) {
+        String rawPrompt = customizedPrompt.getRawPrompt();
+        return chatClient.prompt()
+                .user(rawPrompt)
+                .call()
+                .entity(new ParameterizedTypeReference<>() {
+                });
+    }
+}
---
+++ server/src/main/java/com/nexterview/server/service/SimulatedDialogueGenerator.java
@@ -0,0 +1,27 @@
+package com.nexterview.server.service;
+
+import com.nexterview.server.domain.CustomizedPrompt;
+import com.nexterview.server.domain.PromptComponent;
+import com.nexterview.server.service.dto.response.GeneratedDialogueDto;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+@Profile("local-dev")
+public class SimulatedDialogueGenerator implements DialogueGenerator {
+
+    public List<GeneratedDialogueDto> generate(CustomizedPrompt customizedPrompt) {
+        List<GeneratedDialogueDto> generatedDialogues = new ArrayList<>();
+        generatedDialogues.add(new GeneratedDialogueDto("지시문은?", customizedPrompt.getInstruction()));
+        for (PromptComponent promptComponent : customizedPrompt.getPromptComponents()) {
+            generatedDialogues.add(new GeneratedDialogueDto("질문은: " + promptComponent.getQuery(),
+                    "답변은: " + promptComponent.getAnswer()));
+        }
+
+        return generatedDialogues;
+    }
+}
