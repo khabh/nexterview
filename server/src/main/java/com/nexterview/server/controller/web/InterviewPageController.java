0db9245a6e93202548a4a8bcd6b4edfabf94e3c8
JUHA <84626225+khabh@users.noreply.github.com>
2025-04-03 10:04:14 +0900

    feat: 인터뷰 조회 페이지 구현

--- server/src/main/java/com/nexterview/server/controller/web/InterviewPageController.java
+++ server/src/main/java/com/nexterview/server/controller/web/InterviewPageController.java
@@ -1,13 +1,22 @@
 package com.nexterview.server.controller.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class InterviewPageController {
 
-    @GetMapping("/interview")
-    public String showInterviewPage() {
-        return "interview";
+    @GetMapping("/interview/create")
+    public String showInterviewCreatePage() {
+        return "interview-create";
+    }
+
+    @GetMapping("/interviews/{interviewId}")
+    public String showInterviewPage(@PathVariable Long interviewId, Model model) {
+        model.addAttribute("interviewId", interviewId);
+
+        return "interview-view";
     }
 }
