badb181e0f1892ee3e3ac95265157feb606789fc
juha <khabh@naver.com>
2025-04-14 13:05:24 +0900

    feat: AuthenticationEntryPoint, AccessDeniedHandler 구현

---
+++ server/src/main/java/com/nexterview/server/security/jwt/JwtAccessDeniedHandler.java
@@ -0,0 +1,30 @@
+package com.nexterview.server.security.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nexterview.server.controller.exception.ErrorResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void handle(
+            HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException
+    ) throws IOException, ServletException {
+        ErrorResponse errorResponse = new ErrorResponse("FORBIDDEN", "권한이 없는 사용자입니다.");
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        objectMapper.writeValue(response.getWriter(), errorResponse);
+    }
+}
