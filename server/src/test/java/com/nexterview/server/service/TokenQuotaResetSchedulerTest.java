685e314ffed746d6b1b831d9030de5432b929481
juha <khabh@naver.com>
2025-05-06 10:50:51 +0900

    test: test 프로필 활성화

--- server/src/test/java/com/nexterview/server/service/TokenQuotaResetSchedulerTest.java
+++ server/src/test/java/com/nexterview/server/service/TokenQuotaResetSchedulerTest.java
@@ -12,7 +12,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class TokenQuotaResetSchedulerTest {
 
