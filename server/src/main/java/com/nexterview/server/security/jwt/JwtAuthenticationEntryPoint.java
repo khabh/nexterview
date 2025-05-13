c87711ad15b957dff5a596bf9554fa637ef89313
juha <khabh@naver.com>
2025-05-13 17:25:08 +0900

    refactor: Jwt 인증 실패 시 로깅 추가

--- server/src/main/java/com/nexterview/server/security/jwt/JwtAuthenticationEntryPoint.java
+++ server/src/main/java/com/nexterview/server/security/jwt/JwtAuthenticationEntryPoint.java
@@ -2,17 +2,18 @@ package com.nexterview.server.security.jwt;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nexterview.server.controller.exception.ErrorResponse;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 @RequiredArgsConstructor
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
@@ -21,7 +22,11 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(
             HttpServletRequest request, HttpServletResponse response, AuthenticationException authException
-    ) throws IOException, ServletException {
+    ) throws IOException {
+        String uri = request.getRequestURI();
+        String message = authException.getMessage();
+        log.warn("인증되지 않은 접근 시도: URI={}, message={}", uri, message);
+
         ErrorResponse errorResponse = new ErrorResponse("UNAUTHORIZED", "인증되지 않은 사용자입니다.");
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
