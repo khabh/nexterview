57141d526024d2b699887cfba0c4a060a95a3da5
juha <84626225+khabh@users.noreply.github.com>
2025-05-13 11:23:30 +0900

    view: navbar fragment로 관리 (#50)
    * refactor: 인터뷰 생성 페이지 url 수정
    
    * view: navbar fragment 생성
    
    * view: 공통 head 태그 fragment로 분리
    
    * view: fragment 기존 html에 적용

--- server/src/main/java/com/nexterview/server/controller/web/InterviewPageController.java
+++ server/src/main/java/com/nexterview/server/controller/web/InterviewPageController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class InterviewPageController {
 
-    @GetMapping("/interview/create")
+    @GetMapping("/interviews/create")
     public String showInterviewCreatePage() {
         return "interview-create";
     }
